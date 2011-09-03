df6b861e81c367cc2ea42dbdf504760dd0f01bdc
Travis Hotz <travishotz@gmail.com>
2011-09-03 07:00:34 -0500

    Done with sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   root :to => 'pages#home'
   
   match '/contact', :to => 'pages#contact'
-  match '/about/',  :to => 'pages#about'
+  match '/about',  :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
